82fc2d2959a93d17ad86c603adf8f6a95e1dc652
TomCS3 <62783156+TomCS3@users.noreply.github.com>
2020-10-08 19:02:04 +0100

    3-commerce complete

--- 3-mail/mail/static/mail/inbox.js
+++ 3-mail/mail/static/mail/inbox.js
@@ -26,6 +26,31 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+function send_email() {
+
+  // Retrieves the data from the form
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+
+  // Sends an email via a POST request
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      console.log(result);
+      load_mailbox('sent');
+  });
+  
+  return false;
+}
+
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
@@ -41,43 +66,42 @@ function load_mailbox(mailbox) {
   .then(response => response.json())
   .then(emails => {
     console.log(emails);
-    emails.forEach(email => mailbox_row(email));
+    emails.forEach(email => {
+
+      // Create a div container for one email
+      const box = document.createElement('div');
+      box.setAttribute('id', email.id);
+      box.setAttribute('class', 'container box')
+
+      // Create an attributes row within the container and style
+      const attrs = document.createElement('div');
+      attrs.setAttribute('id', `${email.id}-attrs`);
+      attrs.setAttribute('class', 'row border')
+      attrs.style.backgroundColor = email.read ? '#D3D3D3': 'white';
+      attrs.style.fontWeight = email.read ? 'normal': 'bold';  
+
+      // Create a column for the sender, subject and timestamp
+      const sender = document.createElement('div');
+      sender.setAttribute('class', 'col-lg-3 sender');
+      sender.innerHTML = email.sender;
+      const subject = document.createElement('div');
+      subject.setAttribute('class', 'col-lg-6 subject');
+      subject.innerHTML = email.subject;
+      const timestamp = document.createElement('div');
+      timestamp.setAttribute('class', 'col-lg-3 text-lg-right timestamp');
+      timestamp.innerHTML = email.timestamp;
+
+      // On-click event listner for viewing an email 
+      box.addEventListener('click', () => view_email(email.id));
+
+      // Create the each email's row for the mailbox
+      document.querySelector('#emails-view').append(box);
+      document.getElementById(email.id).appendChild(attrs);
+      document.getElementById(`${email.id}-attrs`).appendChild(sender);
+      document.getElementById(`${email.id}-attrs`).appendChild(subject);
+      document.getElementById(`${email.id}-attrs`).appendChild(timestamp);
+    });
   });
-}
-
-function mailbox_row(email) {
-
-  // Create a div container for one email
-  const box = document.createElement('div');
-  box.setAttribute('id', email.id);
-  box.setAttribute('class', 'container box')
-
-  // Create an attributes row within the container and style
-  const attrs = document.createElement('div');
-  attrs.setAttribute('id', `${email.id}-attrs`);
-  attrs.setAttribute('class', 'row')
-  attrs.style.backgroundColor = email.read ? '#D3D3D3': 'white'; 
-
-  // Create a column for the sender, subject and timestamp
-  const sender = document.createElement('div');
-  sender.setAttribute('class', 'col-3 sender');
-  sender.innerHTML = email.sender;
-  const subject = document.createElement('div');
-  subject.setAttribute('class', 'col-6 subject');
-  subject.innerHTML = email.subject;
-  const timestamp = document.createElement('div');
-  timestamp.setAttribute('class', 'col-3 timestamp');
-  timestamp.innerHTML = email.timestamp;
-
-  // On-click event listner for viewing an email 
-  box.addEventListener('click', () => view_email(email.id));
-
-  // Create the each email's row for the mailbox
-  document.querySelector('#emails-view').append(box);
-  document.getElementById(email.id).appendChild(attrs);
-  document.getElementById(`${email.id}-attrs`).appendChild(sender);
-  document.getElementById(`${email.id}-attrs`).appendChild(subject);
-  document.getElementById(`${email.id}-attrs`).appendChild(timestamp);
 }
 
 function view_email(email_id) {
@@ -109,30 +133,53 @@ function view_email(email_id) {
         })
       });
     }
+
+    // Watch for the reply button being clicked, pre load the compose form
+    document.getElementById('reply_email').addEventListener('click', () => {
+      reply_email(email.sender, email.subject, email.body, email.timestamp);
+    });
+
+    // Add and archive / unarchive button to the inbox / archive mailboxes
+    archive_button(email);
+    
   });
 }
 
-function send_email() {
-
-  // Retrieves the data from the form
-  const recipients = document.querySelector('#compose-recipients').value;
-  const subject = document.querySelector('#compose-subject').value;
-  const body = document.querySelector('#compose-body').value;
-
-  // Sends an email via a POST request
-  fetch('/emails', {
-    method: 'POST',
-    body: JSON.stringify({
-        recipients: recipients,
-        subject: subject,
-        body: body
-    })
-  })
-  .then(response => response.json())
-  .then(result => {
-      console.log(result);
-      load_mailbox('sent');
-  });
+function archive_button(email) {
   
-  return false;
+  // Delete the previous button and log the mailbox
+  document.getElementById('archive-btn-box').innerHTML = '';
+  const mailbox = document.querySelector('h3').innerHTML;
+
+  // If the mailbox is not sent, create the archive button
+  if (mailbox !== 'Sent') {
+    const arch_btn = document.createElement('button');
+    arch_btn.setAttribute('class', `btn btn-sm btn-outline-primary my-2`);
+    arch_btn.setAttribute('id', 'archive-btn');
+    arch_btn.innerHTML = mailbox === 'Inbox' ? 'Archive' : 'Unarchive';
+
+    // If the archive button is clicked, change the archive state and load the inbox
+    arch_btn.addEventListener('click', () => {
+      fetch(`/emails/${email.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            archived: !email.archived
+        })
+      })
+      .then(() => load_mailbox('inbox'));
+    })
+    
+    // Place the archive button on the page
+    document.getElementById('archive-btn-box').append(arch_btn);
+  }
+}
+
+// Preload the compose form with information from the email to reply
+function reply_email(sender, subject, body, timestamp) {
+  compose_email();
+  subject = `Re: ${subject}`;
+  previous_email = `\n\nOn ${timestamp} ${sender} wrote:\n${body}\n`;
+  document.querySelector('#compose-recipients').value = sender;
+  document.querySelector('#compose-subject').value = subject;
+  document.querySelector('#compose-body').value = previous_email;
 }
